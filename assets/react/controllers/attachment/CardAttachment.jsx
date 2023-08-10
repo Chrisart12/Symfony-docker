2fbceccd0c127f43df5e84cc09921654822f26bf
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-08-10 16:03:48 +0200

    feat: add trash icon to card attachment

--- assets/react/controllers/attachment/CardAttachment.jsx
+++ assets/react/controllers/attachment/CardAttachment.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import {Card} from "react-bootstrap";
 import {isImage} from "../../../functions/image";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function CardAttachment({ attachment, showMediaViewer }) {
-
     const formatDate = (date) => {
         return new Date(date).toLocaleDateString();
     }
 
     return (
         <Card className="cursor-pointer h-100" key={attachment.id} onClick={() => showMediaViewer(attachment)}>
+            <FontAwesomeIcon style={{ position: 'absolute', right: 5, top: 5 }} icon={faTrash} />
             {isImage(attachment.originalName)
                 ? <Card.Img className="object-fit-cover" height={96} src={attachment.path} variant="top" width={96} />
                 : <></>
