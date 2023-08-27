d2a620fa71148090e0f34fd2214f6d35b4db65fd
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-08-27 22:15:56 +0200

    Refacto addAttachment stimulus controller

--- assets/controllers/issue_controller.js
+++ assets/controllers/issue_controller.js
@@ -5,38 +5,41 @@ export class IssueController extends Controller {
     /** @type {HTMLInputElement} */
     inputAttachment;
 
+    async addAttachment(e) {
+        /** @type {FileList} */
+        const files = e.target.files;
+
+        if (0 === files.length) {
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('attachment', files[0]);
+
+
+        const response = await fetch(`/issues/${this.component.getData('issue')}/attachments`, {
+            body: formData,
+            headers: {
+                'Accept': 'application/json',
+            },
+            method: 'POST'
+        });
+
+        const attachment = await response.json();
+
+        await this.component.action('addAttachment', attachment);
+    }
+
     async initialize() {
         this.component = await getComponent(this.element);
         this.inputAttachment = document.querySelector('#inputAttachment');
 
-
         this.inputAttachment.addEventListener('change', async (e) => {
-            /** @type {FileList} */
-            const files = e.target.files;
-
-            if (0 === files.length) {
-                return;
-            }
-
-            const formData = new FormData();
-            formData.append('attachment', files[0]);
-
-
-            const response = await fetch(`/issues/${this.component.getData('issue')}/attachments`, {
-                body: formData,
-                headers: {
-                    'Accept': 'application/json',
-                },
-                method: 'POST'
-            });
-
-            const attachment = await response.json();
-
-            await this.component.action('addAttachment', attachment);
+            await this.addAttachment(e);
         });
     }
 
-    addAttachment(e) {
+    openFileDialog() {
         this.inputAttachment.click();
     }
 }
