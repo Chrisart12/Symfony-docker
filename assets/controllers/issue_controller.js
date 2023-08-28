149a43094acb8e32c12c1a6df02cd56ba832a84b
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-08-28 16:41:07 +0200

    feat: update buttonAttach disabled state according to inputAttachment files length

--- assets/controllers/issue_controller.js
+++ assets/controllers/issue_controller.js
@@ -2,7 +2,26 @@ import { Controller } from '@hotwired/stimulus';
 import { getComponent } from '@symfony/ux-live-component';
 
 export class IssueController extends Controller {
+    /** @type {HTMLButtonElement} */
+    buttonAttach;
+
+    /** @type {HTMLInputElement} */
+    inputAttachment;
+
+    async updateButtonAttachDisabledState(e) {
+        /** @type {FileList} */
+        const files = e.target.files;
+
+        this.buttonAttach.disabled = 0 === files.length;
+    }
+
     async initialize() {
         this.component = await getComponent(this.element);
+        this.buttonAttach = document.querySelector('#buttonAttach');
+        this.inputAttachment = document.querySelector('#inputAttachment');
+
+        this.inputAttachment.addEventListener('change', async (e) => {
+            await this.updateButtonAttachDisabledState(e);
+        });
     }
 }
